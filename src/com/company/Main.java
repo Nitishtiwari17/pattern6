cf5b2054c5ea7a5aa90f13d0cd323f85594c9454
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2022-02-14 23:45:12 +0530

    variable name change.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int n=65;
-        for(int i=0;i<n;i++)
+        int a=65;
+        for(int i=0;i<6;i++)
         {
             for(int k=5;k>i;k--)
             {
@@ -12,7 +12,7 @@ public class Main {
             }
             for(int j=0;j<i;j++)
             {
-                System.out.print((char)(n+j)+" ");
+                System.out.print((char)(a+j)+" ");
             }
             System.out.println(" ");
         }
